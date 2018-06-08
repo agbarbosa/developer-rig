30458e5cde279b21c3cb08e51a408fa0900fb478
David Peppel <skeltonath@gmail.com>
2018-06-08 13:42:09 -0700

    more fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Rig } from './rig';
-import { RigStore } from './core/rig-store';
 import { wrap } from './util/react';
-
-export const store = new RigStore();
+import { store } from './core/rig';
 
 ReactDOM.render(wrap(store, <Rig />), document.getElementById('root'));
